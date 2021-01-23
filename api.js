86b966d6924cbbe1c701c80584a675ee7084a720
SebastianCaballero <sebastian.caballero7@outlook.com>
2021-01-23 02:25:52 -0500

    Number of questions and categories added

---
+++ api.js
@@ -0,0 +1,43 @@
+function getData() {
+    let totalQuestions = document.getElementById('questions').value;
+    let category = document.getElementById('control').value;
+
+    fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&category=${category}`)
+    .then(response => response.json())
+    .then(data => printData(data));
+}
+
+function getCategories() {
+    const url = 'https://opentdb.com/api_category.php';
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => printCategories(data.trivia_categories));
+    
+}
+
+
+function printData(data) {
+    let content = document.getElementById('questions-container');
+
+    data.results.forEach((it) => {
+        content.innerHTML += `<div class="col-md-4">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            ${it.question}
+                                        </div>
+                                    </div>
+                                </div>`
+    });
+}
+
+function printCategories(data) {
+    let content = document.getElementById('control');
+
+    data.forEach((category) => {
+        content.innerHTML += `<option value="${category.id}">${category.name}</option>`;
+    })
+}
+
+getCategories();
+
